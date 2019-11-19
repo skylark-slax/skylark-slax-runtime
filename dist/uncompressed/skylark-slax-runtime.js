0a98a66d3b95533b8876b0144746e36ea2d93719
hudaokeji <github@hudaokeji.com>
2019-11-19 09:52:05 +0900

    update libraries

--- dist/uncompressed/skylark-slax-runtime.js
+++ dist/uncompressed/skylark-slax-runtime.js
@@ -226,7 +226,18 @@ define('skylark-slax-runtime/main',[
 	"skylark-langx",
 	"skylark-widgets-shells",
 	"skylark-jquery",
-	"skylark-ajaxfy-spa"
+	"skylark-ajaxfy-spa",
+	"skylark-data-entities",
+	"skylark-data-streams",
+	"skylark-data-zip",
+	"skylark-net-http",
+	"skylark-widgets-colorpicker",
+	"skylark-widgets-gradienter",
+	"skylark-widgets-hierarchy",
+	"skylark-widgets-iconpicker",
+	"skylark-widgets-repeater",
+	"skylark-widgets-uploads",
+	"skylark-widgets-wordpad"
 ],function(slax){
 	return slax;
 });
