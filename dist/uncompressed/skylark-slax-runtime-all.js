4835b6bc4f699e6f2452d883c00e7255eff176b0
hudaokeji <github@hudaokeji.com>
2020-01-02 09:45:51 +0900

    update libraries

--- dist/uncompressed/skylark-slax-runtime-all.js
+++ dist/uncompressed/skylark-slax-runtime-all.js
@@ -86102,6 +86102,7 @@ define('skylark-widgets-wordpad/addons/actions/HtmlAction',[
         lineNumbers: true
   
         };
+       this.editor.sync(); 
        if (!this.CodeMirrorEditor) {
          this.CodeMirrorEditor = CodeMirror.fromTextArea(this.editor.textarea[0], codemirrorOptions);
          this.CodeMirrorEditor.on("blur",function(){
@@ -88327,7 +88328,6 @@ define('skylark-widgets-wordpad/addons/actions/VideoAction',[
           }).attr({
             "width" : videoData.width + "px",
             "height" : videoData.height + "px",
-
             'class' : 'wordpad-video',
             "data-link" : videoData.link,
             "data-width" : videoData.width,
