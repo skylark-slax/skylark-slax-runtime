f552110c2ce3946c76c4149e6dc0970400940361
hudaokeji <github@hudaokeji.com>
2020-05-09 13:00:02 +0900

    update

--- dist/skylark-slax-runtime/main.js
+++
@@ -1,9 +0,0 @@
-/**
- * skylark-slax-runtime - The skylark shells widget
- * @author Hudaokeji, Inc.
- * @version v0.9.0
- * @link https://github.com/skylark-slax/skylark-slax-runtime/
- * @license MIT
- */
-define(["./slax","./cache","skylark-langx","skylark-widgets-shells","skylark-jquery","skylark-ajaxfy-spa","skylark-data-entities","skylark-data-streams","skylark-data-zip","skylark-data-zip","skylark-domx-images","skylark-widgets-colorpicker","skylark-widgets-gradienter","skylark-widgets-hierarchy","skylark-widgets-iconpicker","skylark-widgets-repeater","skylark-widgets-uploads","skylark-widgets-wordpad"],function(a){return a});
-//# sourceMappingURL=sourcemaps/main.js.map
--- dist/skylark-slax-runtime/models.js
+++
@@ -1,9 +0,0 @@
-/**
- * skylark-slax-runtime - The skylark shells widget
- * @author Hudaokeji, Inc.
- * @version v0.9.0
- * @link https://github.com/skylark-slax/skylark-slax-runtime/
- * @license MIT
- */
-define(["./slax"],function(e){var n={};return defien("models",[],function(){return n}),e.models=n});
-//# sourceMappingURL=sourcemaps/models.js.map
